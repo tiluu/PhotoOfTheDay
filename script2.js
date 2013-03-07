7db0d92e4c83754ae5a492ef297675fa6ff3d6d0
tina.p.luu@gmail.com <tina.p.luu@gmail.com>
2013-03-07 17:18:48 -0500

    added cities

--- script2.js
+++ script2.js
@@ -1,12 +1,14 @@
 jQuery(function($){
 	var access_token = '4112123.f59def8.e61dd790f91c48e6803d18459723906a';
-	var insta_url= "https://api.instagram.com/v1/locations/search?foursquare_v2_id=4acf58baf964a52028d320e3";
+	var location;
+	var insta_url= "https://api.instagram.com/v1/locations/search?foursquare_v2_id=" ;
 	var next;
 	var insta_photo;
-	
-//Retrives photos from location and returns with location ids
-	function getLocation(){
-		$.ajax(insta_url, {
+
+
+//Retrives photos from location and returns with location ids lat=48.858844&lng=2.294351&distance=5000
+	function getLocation(location){
+		$.ajax(insta_url+location, {
 			data: {'access_token':access_token},
 			dataType:'jsonp',
 			success: dataLoaded
@@ -44,9 +46,9 @@ jQuery(function($){
 			tag=data.tags
 			for(i=0; i<=tag.length; i++){
 				if(tag[i]=="photooftheday"){
-					photo = "<li><a href='" +data.link+ "'><img src='" +data.images.standard_resolution.url + "'</img></a></li>";
+					photo = "<li><a href='" +data.link+ "'target=_blank><img src='" +data.images.standard_resolution.url + "'</img></a></li>";
 					$('#target').append(photo);	
-					imageCount = true
+				imageCount = true
 				}
 			}
 		});
@@ -55,8 +57,57 @@ jQuery(function($){
 			grab(next);
 		}
 	}
+
+	$('div#city button').click(function(){
+		var city= $(this).attr('id');
+		switch(city)
+		{
+			case "nyc":
+			location="4acf58baf964a52028d320e3";
+			break;
+
+			case "sf":
+			location="4c82f252d92ea09323185072";
+			break;
+
+			case "london":
+			location="4e10c02045dde6c62dcd8b19";
+			break;
+
+			case "rome":
+			location= "4e8bab8fb803540e781e7d03";
+			break;
+
+			case "bangkok":
+			location= "50b5e9f7e4b009224db8f53d";
+			break;
+
+			case "madrid":
+			location="4d683074d4c288bf50da7065";
+			break;
+
+			case "bangkok":
+			location= "4d21b7010901721e2ff29ca5";
+			break;
+
+			case "mexico":
+			location="4f3b7619e4b08082ae2bb300";
+			break;
+			case "sydney":
+			location="5081ca91e4b0607ce86717c3";
+			break;
+
+			case "telaviv":
+			location="4c4a3ca59c8d2d7febc63969";
+			break;
+
+			case "saopaulo":
+			location = "4ce1b9a37e2e236af1e4911b";
+			break;
+		}
+		getLocation(location);
+	})
 	
-	getLocation();
 	$('#next').click(function(){
 		grab(next);
 	});
